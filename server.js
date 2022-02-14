598105590c1e54e31351621be8ebc01f810c9389
Mike <mikej.miller440@gmail.com>
2022-02-13 20:11:36 -0600

    added database connection

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const dotenv = require('dotenv');
 const cTable = require('console.table');
+
+const PORT = 3001;
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected');
+    append.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
